5bb72179e5942998616e3d57563ddbbc73c07795
landon <sbshz85@gmail.com>
2019-05-08 00:07:25 +0900

    make HomeController, ejs view

--- src/controllers/api/V1/HomeController.js
+++ src/controllers/api/V1/HomeController.js
@@ -1,1 +1,10 @@
-// HomeController.js
+const HomeController = {
+  index: (req, res) => {
+    res.render('index', {
+      title: 'Hello hyundo',
+      length: 5,
+    });
+  },
+};
+
+export default HomeController;
